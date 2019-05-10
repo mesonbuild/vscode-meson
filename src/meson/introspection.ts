9c502dcc0f7132ac981628dac852718f0664dd47
Nathan Graule <solarliner@gmail.com>
2019-05-10 18:50:45 +0200

    Add function getting and parsing Meson version

--- src/meson/introspection.ts
+++ src/meson/introspection.ts
@@ -1,6 +1,8 @@
 import { exec } from "../utils";
 import { Targets, Dependencies, BuildOptions, Test } from "./types";
 
+const MESON_VERSION_REGEX = /^(\d+)\.(\d+)\.(\d+)/g;
+
 export async function getMesonTargets(build: string) {
   const { stdout } = await exec("meson introspect --targets", {
     cwd: build
@@ -21,4 +23,19 @@ export async function getMesonDependencies(build: string) {
 }
 export async function getMesonTests(build: string) {
   return [] as Test[];
+}
+
+export async function getMesonVersion(): Promise<[number, number, number]> {
+  const { stdout } = await exec("meson --version", {});
+  if (MESON_VERSION_REGEX.test(stdout.trim())) {
+    const match = MESON_VERSION_REGEX.exec(stdout.trim());
+    return match.slice(1, 3).map(s => Number.parseInt(s)) as [
+      number,
+      number,
+      number
+    ];
+  } else
+    throw new Error(
+      "Meson version doesn't match expected output: " + stdout.trim()
+    );
 }
