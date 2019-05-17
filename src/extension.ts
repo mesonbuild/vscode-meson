bb337e7390df90bc26b7fc25a46398fe9dbc0e6f
Nathan Graule <solarliner@gmail.com>
2019-05-17 19:22:21 +0200

    Add target selector to build command
    The picker will only appear if no target has been given to the command
    already - as is the case when invoking the command from the Command Prompt.

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ import {
 import { getMesonTasks } from "./tasks";
 import { MesonProjectExplorer } from "./treeview";
 import { extensionConfiguration, execAsTask, workspaceRelative } from "./utils";
+import { getMesonTargets } from "./meson/introspection";
 
 let explorer: MesonProjectExplorer;
 
@@ -19,16 +20,18 @@ export function activate(ctx: vscode.ExtensionContext): void {
 
   explorer = new MesonProjectExplorer(ctx, buildDir);
 
-  const tasksDisposable = vscode.tasks.registerTaskProvider("meson", {
-    provideTasks(token) {
-      return getMesonTasks(
-        workspaceRelative(extensionConfiguration("buildFolder"))
-      );
-    },
-    resolveTask() {
-      return undefined;
-    }
-  });
+  ctx.subscriptions.push(
+    vscode.tasks.registerTaskProvider("meson", {
+      provideTasks(token) {
+        return getMesonTasks(
+          workspaceRelative(extensionConfiguration("buildFolder"))
+        );
+      },
+      resolveTask() {
+        return undefined;
+      }
+    })
+  );
 
   ctx.subscriptions.push(
     vscode.commands.registerCommand("mesonbuild.configure", async () => {
@@ -51,7 +54,43 @@ export function activate(ctx: vscode.ExtensionContext): void {
       async (name?: string) => {
         await runMesonBuild(
           workspaceRelative(extensionConfiguration("buildFolder")),
-          name
+          await new Promise((resolve, reject) => {
+            if (name) {
+              return resolve(name);
+            }
+            const itemsP: Promise<vscode.QuickPickItem[]> = getMesonTargets(
+              buildDir
+            ).then<vscode.QuickPickItem[]>(tt => [
+              {
+                label: "all",
+                detail: "Build all targets",
+                description: "(meta-target)",
+                picked: true
+              },
+              ...tt.map<vscode.QuickPickItem>(t => ({
+                label: t.name,
+                detail: path.relative(root, path.dirname(t.defined_in)),
+                description: t.type,
+                picked: false
+              }))
+            ]);
+            const picker = vscode.window.createQuickPick();
+            picker.busy = true;
+            picker.placeholder =
+              "Select target to build. Defaults to all targets";
+            picker.show();
+            itemsP.then(items => {
+              picker.items = items;
+              picker.busy = false;
+              picker.onDidAccept(() => {
+                const active = picker.activeItems[0];
+                if (active.label === "all") resolve(undefined);
+                else resolve(path.join(active.detail, active.label));
+                picker.dispose();
+              });
+              picker.onDidHide(() => reject());
+            });
+          })
         );
         explorer.refresh();
       }
@@ -75,8 +114,6 @@ export function activate(ctx: vscode.ExtensionContext): void {
       });
     })
   );
-
-  ctx.subscriptions.push(tasksDisposable);
 
   if (extensionConfiguration("configureOnOpen"))
     vscode.commands
