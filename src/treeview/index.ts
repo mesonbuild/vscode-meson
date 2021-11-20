mesonbuild/vscode-meson

commit b24c9a235986a1e64a17ec7b3ca29f1083c7b54e

Move everything to one master treeview

src/treeview/index.ts
@@ -1,0 +1,49 @@
+import * as vscode from "vscode";
+import { BaseNode } from "./nodes/base";
+import { getMesonProjectInfo } from "../meson/introspection";
+import { ProjectNode } from "./nodes/toplevel";
+
+export class MesonProjectDataProvider
+  implements vscode.TreeDataProvider<BaseNode> {
+  private _onDataChangeEmitter = new vscode.EventEmitter<BaseNode>();
+  readonly onDidChangeTreeData = this._onDataChangeEmitter.event;
+
+  constructor(ctx: vscode.ExtensionContext, private buildDir: string) {
+    ctx.subscriptions.push(
+      vscode.commands.registerCommand("mesonbuild.view-refresh", () =>
+        this.refresh()
+      )
+    );
+  }
+
+  refresh() {
+    this._onDataChangeEmitter.fire();
+  }
+
+  getTreeItem(element: BaseNode) {
+    return element.getTreeItem();
+  }
+  async getChildren(element?: BaseNode) {
+    if (element) return element.getChildren();
+    return [
+      await getMesonProjectInfo(this.buildDir).then(
+        p => new ProjectNode(p, this.buildDir)
+      )
+    ];
+  }
+}
+
+export class MesonProjectExplorer {
+  private viewer: vscode.TreeView<BaseNode>;
+
+  constructor(ctx: vscode.ExtensionContext, buildDir: string) {
+    const treeDataProvider = new MesonProjectDataProvider(ctx, buildDir);
+    this.viewer = vscode.window.createTreeView("meson-project", {
+      treeDataProvider
+    });
+  }
+
+  public refresh() {
+    vscode.commands.executeCommand("mesonbuild.targets-refresh");
+  }
+}

src/extension.ts
@@ -1,7 +1,5 @@
 import * as path from "path";
 import * as vscode from "vscode";
-import { MesonTargetsExplorer } from "./meson/targets/explorer";
-import { MesonTestsExplorer } from "./meson/tests/explorer";
 import {
   runMesonConfigure,
   runMesonBuild,
@@ -9,9 +7,9 @@ import {
   runMesonReconfigure
 } from "./meson/runners";
 import { getMesonTasks } from "./tasks";
+import { MesonProjectExplorer } from "./treeview";
 
-let targetExplorer: MesonTargetsExplorer | undefined;
-let testExplorer: MesonTestsExplorer | undefined;
+let explorer: MesonProjectExplorer;
 
 export function activate(ctx: vscode.ExtensionContext): void {
   const root = vscode.workspace.rootPath;
@@ -19,8 +17,7 @@ export function activate(ctx: vscode.ExtensionContext): void {
   const buildDir = path.join(root, "build");
   if (!root) return;
 
-  targetExplorer = new MesonTargetsExplorer(ctx, buildDir);
-  testExplorer = new MesonTestsExplorer(ctx, buildDir);
+  explorer = new MesonProjectExplorer(ctx, buildDir);
 
   const tasksDisposable = vscode.tasks.registerTaskProvider("meson", {
     provideTasks(token) {
@@ -34,15 +31,13 @@ export function activate(ctx: vscode.ExtensionContext): void {
   ctx.subscriptions.push(
     vscode.commands.registerCommand("mesonbuild.configure", async () => {
       await runMesonConfigure(root, buildDir);
-      targetExplorer.refresh();
-      testExplorer.refresh();
+      explorer.refresh();
     })
   );
   ctx.subscriptions.push(
     vscode.commands.registerCommand("mesonbuild.reconfigure", async () => {
       await runMesonReconfigure();
-      targetExplorer.refresh();
-      testExplorer.refresh();
+      explorer.refresh();
     })
   );
   ctx.subscriptions.push(
@@ -50,8 +45,7 @@ export function activate(ctx: vscode.ExtensionContext): void {
       "mesonbuild.build",
       async (name?: string) => {
         await runMesonBuild(name);
-        targetExplorer.refresh();
-        testExplorer.refresh();
+        explorer.refresh();
       }
     )
   );
@@ -69,7 +63,6 @@ export function activate(ctx: vscode.ExtensionContext): void {
   vscode.commands
     .executeCommand<boolean>("mesonbuild.configure")
     .then(isFresh => {
-      targetExplorer.refresh();
-      testExplorer.refresh();
+      explorer.refresh();
     });
 }

src/meson/introspection.ts
@@ -31,7 +31,7 @@ export async function getMesonTargets(build: string) {
   return parsed;
 }
 export async function getMesonBuildOptions(build: string) {
-  const parsed = await parseJSONFileIfExists(
+  const parsed = await parseJSONFileIfExists<BuildOptions>(
     path.join(build, "meson-info/intro-buildoptions.json")
   );
   if (parsed) return parsed;
@@ -43,7 +43,7 @@ export async function getMesonBuildOptions(build: string) {
 }
 
 export async function getMesonProjectInfo(build: string) {
-  const parsed = parseJSONFileIfExists<ProjectInfo>(
+  const parsed = await parseJSONFileIfExists<ProjectInfo>(
     path.join(build, "meson-info/intro-projectinfo.json")
   );
   if (parsed) return parsed;

src/meson/targets/explorer.ts
@@ -1,17 +1,0 @@
-import * as vscode from "vscode";
-import { MesonTargetsDataProvider } from "./provider";
-import { BaseNode } from "../basenode";
-
-export class MesonTargetsExplorer {
-  private viewer: vscode.TreeView<BaseNode>;
-
-  constructor(ctx: vscode.ExtensionContext, build: string) {
-    const treeDataProvider = new MesonTargetsDataProvider(build);
-    this.viewer = vscode.window.createTreeView("meson-targets", {
-      treeDataProvider
-    });
-  }
-  public refresh() {
-    vscode.commands.executeCommand("mesonbuild.targets-refresh");
-  }
-}

src/meson/targets/provider.ts
@@ -1,32 +1,0 @@
-import * as vscode from "vscode";
-import { BaseNode } from "../basenode";
-import { getMesonTargets } from "../introspection";
-import { TargetDirectoryNode } from "../../treeview/nodes/targets";
-
-export class MesonTargetsDataProvider
-  implements vscode.TreeDataProvider<BaseNode> {
-  private _onDataChangeEmitter = new vscode.EventEmitter<BaseNode>();
-  readonly onDidChangeTreeData = this._onDataChangeEmitter.event;
-
-  constructor(private buildDir: string) {
-    vscode.commands.registerCommand("mesonbuild.targets-refresh", () =>
-      this.refresh()
-    );
-  }
-
-  refresh() {
-    this._onDataChangeEmitter.fire();
-  }
-
-  getTreeItem(element: BaseNode): vscode.ProviderResult<vscode.TreeItem> {
-    return element.getTreeItem();
-  }
-
-  getChildren(element?: BaseNode): vscode.ProviderResult<BaseNode[]> {
-    if (element) return element.getChildren();
-    else
-      return getMesonTargets(this.buildDir).then(targets =>
-        new TargetDirectoryNode(".", targets).getChildren()
-      );
-  }
-}

src/treeview/nodes/base.ts
@@ -9,11 +9,11 @@ import { TreeItem } from "vscode";
 
 export class BaseNode {
   constructor(public readonly id: string) {}
-  getChildren(): BaseNode[] {
+  getChildren(): vscode.ProviderResult<BaseNode[]> {
     return [];
   }
 
-  getTreeItem() {
+  getTreeItem(): vscode.ProviderResult<TreeItem> {
     const item = new TreeItem(this.id);
     item.id = this.id;
     return item;
@@ -29,7 +29,7 @@ export abstract class BaseDirectoryNode<T> extends BaseNode {
   }
 
   getTreeItem() {
-    const item = super.getTreeItem();
+    const item = super.getTreeItem() as vscode.TreeItem;
     // item.resourceUri = vscode.Uri.file(this.folder);
     item.label = path.basename(this.folder);
     item.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;

src/treeview/nodes/sources.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import * as vscode from "vscode";
 
 import { extensionRelative, randomString } from "../../utils";
-import { BaseNode } from "../../meson/basenode";
+import { BaseNode } from "./base";
 import { BaseFileDirectoryNode } from "./base";
 
 export class TargetSourcesNode extends BaseFileDirectoryNode {
@@ -11,7 +11,7 @@ export class TargetSourcesNode extends BaseFileDirectoryNode {
   }
 
   getTreeItem() {
-    const item = super.getTreeItem();
+    const item = super.getTreeItem() as vscode.TreeItem;
     item.label = "Sources" + (this.allFiles.length === 0 ? " (no files)" : "");
     item.iconPath = extensionRelative("res/meson_64.svg");
     return item;
@@ -24,7 +24,7 @@ export class TargetGeneratedSourcesNode extends BaseFileDirectoryNode {
   }
 
   getTreeItem() {
-    const item = super.getTreeItem();
+    const item = super.getTreeItem() as vscode.TreeItem;
     item.label = "Sources (generated)";
     item.iconPath = extensionRelative("res/meson_64.svg");
     return item;
@@ -50,7 +50,7 @@ export class TargetSourceFileNode extends BaseNode {
   }
 
   getTreeItem() {
-    const item = super.getTreeItem();
+    const item = super.getTreeItem() as vscode.TreeItem;
     item.resourceUri = vscode.Uri.file(this.file);
     item.label = path.basename(this.file);
     item.command = {

src/treeview/nodes/targets.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import * as vscode from "vscode";
 
-import { BaseNode } from "../../meson/basenode";
+import { BaseNode } from "./base";
 import { Target, Targets } from "../../meson/types";
 import { TargetSourcesNode, TargetGeneratedSourcesNode } from "./sources";
 import { extensionRelative, getTargetName } from "../../utils";
@@ -62,7 +62,7 @@ export class TargetNode extends BaseNode {
     }
   }
   getTreeItem() {
-    const item = super.getTreeItem();
+    const item = super.getTreeItem() as vscode.TreeItem;
     item.iconPath = extensionRelative(this.getIconPath());
     item.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
     item.label = getTargetName(this.target);

commit 42124a726c732001a47fa5ef6666d0e4f0adef0a

fix: wrong command name used by MesonProjectExplorer.refresh().
Broken in b24c9a23 by the looks of it.

src/treeview/index.ts
@@ -3,14 +3,15 @@ import { BaseNode } from "./basenode";
 import { getMesonProjectInfo } from "../meson/introspection";
 import { ProjectNode } from "./nodes/toplevel";
 
-export class MesonProjectDataProvider
-  implements vscode.TreeDataProvider<BaseNode> {
-  private _onDataChangeEmitter = new vscode.EventEmitter<BaseNode>();
+class MesonProjectDataProvider implements vscode.TreeDataProvider<BaseNode> {
+  private readonly _onDataChangeEmitter = new vscode.EventEmitter<BaseNode>();
   readonly onDidChangeTreeData = this._onDataChangeEmitter.event;
 
-  constructor(ctx: vscode.ExtensionContext, private buildDir: string) {
+  static readonly commandName = "mesonbuild.view-refresh";
+
+  constructor(ctx: vscode.ExtensionContext, private readonly buildDir: string) {
     ctx.subscriptions.push(
-      vscode.commands.registerCommand("mesonbuild.view-refresh", () =>
+      vscode.commands.registerCommand(MesonProjectDataProvider.commandName, () =>
         this.refresh()
       )
     );
@@ -23,18 +24,19 @@ export class MesonProjectDataProvider
   getTreeItem(element: BaseNode) {
     return element.getTreeItem();
   }
+
   async getChildren(element?: BaseNode) {
-    if (element) return element.getChildren();
-    return [
-      await getMesonProjectInfo(this.buildDir).then(
-        p => new ProjectNode(p, this.buildDir)
-      )
-    ];
+    if (element) {
+      return element.getChildren();
+    }
+
+    const projectInfo = await getMesonProjectInfo(this.buildDir);
+    return [new ProjectNode(projectInfo, this.buildDir)];
   }
 }
 
 export class MesonProjectExplorer {
-  private viewer: vscode.TreeView<BaseNode>;
+  private readonly viewer: vscode.TreeView<BaseNode>;
 
   constructor(ctx: vscode.ExtensionContext, buildDir: string) {
     const treeDataProvider = new MesonProjectDataProvider(ctx, buildDir);
@@ -44,6 +46,6 @@ export class MesonProjectExplorer {
   }
 
   public refresh() {
-    vscode.commands.executeCommand("mesonbuild.targets-refresh");
+    vscode.commands.executeCommand(MesonProjectDataProvider.commandName);
   }
 }