3a29a06bbcdfbfa38e8ec54063e67a1e0ff03883
Markus Ongyerth <ongy@ongy.net>
2022-02-11 08:54:26 +0100

    Make test debug runner compile dependencies

--- src/tests.ts
+++ src/tests.ts
@@ -10,7 +10,8 @@ import {
 } from "./meson/types"
 import {
     getMesonTests,
-    getMesonTestLogs
+    getMesonTestLogs,
+    getMesonTargets
 } from "./meson/introspection"
 
 export async function rebuildTests(controller: vscode.TestController) {
@@ -48,6 +49,8 @@ export async function testRunHandler(controller: vscode.TestController, request:
         await exec('meson', args);
     } catch(e) {
         if (e.error.code == 125) {
+            /* If desired, we could probably do this in a way that we fault every test in this error case.
+             * Might be better semantically but at the same time, those tests didn't fail. */
             vscode.window.showErrorMessage("Failed to build tests. Results will not be updated");
         }
         run.appendOutput(e.stdout);
@@ -84,22 +87,40 @@ export async function testDebugHandler(controller: vscode.TestController, reques
     }
 
     const tests: Tests = await getMesonTests(workspaceRelative(extensionConfiguration("buildFolder")));
-    for (let test of queue) {
-        for (let config of tests) {
-        if (test.id == config.name) {
-            let args = [...config.cmd]
-            args.shift();
-            await vscode.debug.startDebugging(undefined, {
-                    name: `meson-debug-${test.id}`,
-                    type: "cppdbg",
-                    request: "launch",
-                    cwd: config.workdir || workspaceRelative(extensionConfiguration("buildFolder")),
-                    env: config.env,
-                    program: config.cmd[0],
-                    args: args,
-                });
-            }
-        }
+    const targets = await getMesonTargets(workspaceRelative(extensionConfiguration("buildFolder")));
+
+    /* while meson has the --gdb arg to test, but IMO we should go the actual debugger route.
+     * We still want stuff to be built though... Without going through weird dances */
+    const relevantTests = tests.filter(test => queue.some(candidate => candidate.id == test.name));
+    const requiredTargets = targets.filter(target => relevantTests.some(test => test.depends.some(dep => dep == target.id)));
+
+    var args = ['compile', '-C', workspaceRelative(extensionConfiguration("buildFolder"))]
+    requiredTargets.forEach(target => {
+        args.push(target.name);
+    });
+
+    try {
+        await exec('meson', args);
+    } catch(e) {
+        vscode.window.showErrorMessage("Failed to build tests. Results will not be updated");
+        run.end();
+        return;
+    }
+
+    /* We already figured out which tests we want to run.
+     * We don't use the actual test either way, as we don't get the result here... */
+    for (let test of relevantTests) {
+        let args = [...test.cmd]
+        args.shift();
+        await vscode.debug.startDebugging(undefined, {
+                name: `meson-debug-${test.name}`,
+                type: "cppdbg",
+                request: "launch",
+                cwd: test.workdir || workspaceRelative(extensionConfiguration("buildFolder")),
+                env: test.env,
+                program: test.cmd[0],
+                args: args,
+            });
     }
 
     run.end();
