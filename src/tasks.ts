mesonbuild/vscode-meson

commit de6d4e91c7f8c5c74c5fb3756937c7cb1a37d847

Enable strict mode and fix up all errors and warnings.
This is the current tsconfig from generator-code with the extra options enabled
(and noPropertyAccessFromIndexSignature).

src/tasks.ts
@@ -70,7 +70,7 @@ function createReconfigureTask(buildDir: string) {
   );
 }
 
-export async function getMesonTasks(buildDir: string): Promise<vscode.Task[]> {
+export async function getMesonTasks(buildDir: string) {
   try {
     const defaultBuildTask = new vscode.Task(
       { type: "meson", mode: "build" },
@@ -119,8 +119,9 @@ export async function getMesonTasks(buildDir: string): Promise<vscode.Task[]> {
     ];
 
     // Remaining tasks needs a valid configuration
-    if (!checkMesonIsConfigured(buildDir))
+    if (!checkMesonIsConfigured(buildDir)) {
       return tasks;
+    }
 
     const [targets, tests, benchmarks] = await Promise.all([
       getMesonTargets(buildDir),
@@ -159,9 +160,14 @@ export async function getMesonTasks(buildDir: string): Promise<vscode.Task[]> {
       ...benchmarks.map(b => createTestTask(b, buildDir, true))
     );
     return tasks;
-  } catch (e) {
-    getOutputChannel().appendLine(e);
-    if (e.stderr) getOutputChannel().appendLine(e.stderr);
+  } catch (e: any) {
+    if ("error" in e) {
+      getOutputChannel().appendLine(e.error.message);
+    }
+    if ("stderr" in e) {
+      getOutputChannel().appendLine(e.stderr);
+    }
+
     vscode.window.showErrorMessage(
       "Could not fetch targets. See Meson Build output tab for more info."
     );
@@ -173,27 +179,36 @@ export async function getMesonTasks(buildDir: string): Promise<vscode.Task[]> {
 export async function getTask(mode: string, name?: string) {
   const tasks = await vscode.tasks.fetchTasks({ type: "meson" });
   const filtered = tasks.filter(
-    t => t.definition.mode === mode && (!name || t.definition.target === name)
+    (t) => t.definition["mode"] === mode && !name || t.definition["target"] === name
   );
-  if (filtered.length === 0)
+  if (filtered.length === 0) {
     throw new Error(`Cannot find ${mode} target ${name}.`);
+  }
+
   return filtered[0];
 }
 
 export async function getTasks(mode: string) {
   const tasks = await vscode.tasks.fetchTasks({ type: "meson" });
   return tasks.filter(
-    t => t.definition.mode === mode
+    (t) => t.definition["mode"] === mode
   );
 }
 
 export async function runTask(task: vscode.Task) {
   try {
     await vscode.tasks.executeTask(task);
-  } catch (e) {
-    vscode.window.showErrorMessage(`Could not ${task.definition.mode} ${task.name}`);
+  } catch (e: any) {
+    vscode.window.showErrorMessage(`Could not ${task.definition["mode"]} ${task.name}`);
     getOutputChannel().appendLine(`Running task ${task.name}:`);
-    getOutputChannel().appendLine(e);
+
+    if ("error" in e) {
+      getOutputChannel().appendLine(e.error.message);
+    }
+    if ("stderr" in e) {
+      getOutputChannel().appendLine(e.stderr);
+    }
+
     getOutputChannel().show(true);
   }
 }

src/configprovider.ts
@@ -38,8 +38,8 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
 
   async createGDBDebugConfiguration(target: Target): Promise<vscode.DebugConfiguration> {
     let debugConfig = await this.createBaseDebugConfiguration(target);
-    debugConfig.MIMode = MIModes.gdb;
-    debugConfig.setupCommands = [{
+    debugConfig["MIMode"] = MIModes.gdb;
+    debugConfig["setupCommands"] = [{
       description: 'Enable pretty-printing for gdb',
       text: '-enable-pretty-printing',
       ignoreFailures: true
@@ -49,7 +49,7 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
 
   async createLLDBDebugConfiguration(target: Target): Promise<vscode.DebugConfiguration> {
     let debugConfig = await this.createBaseDebugConfiguration(target);
-    debugConfig.MIMode = MIModes.lldb;
+    debugConfig["MIMode"] = MIModes.lldb;
     return debugConfig;
   }
 
@@ -68,7 +68,7 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
     let ret: vscode.DebugConfiguration[] = [];
 
     for (const target of executables) {
-      if (!target.target_sources.some(source => ['cpp', 'c'].includes(source.language))) {
+      if (!target.target_sources?.some(source => ['cpp', 'c'].includes(source.language))) {
         continue;
       }
 

src/extension.ts
@@ -238,7 +238,7 @@ export async function activate(ctx: vscode.ExtensionContext) {
       runFirstTask(mode, name);
       return;
     }
-    let taskItem: TaskQuickPickItem;
+    let taskItem;
     try {
       taskItem = await pickTask(mode);
     } catch (err) {

src/formatters.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 import {
   extensionConfiguration,
   getOutputChannel
@@ -28,7 +27,7 @@ const formatters: Record<string, FormatterDefinition> = {
 }
 
 async function reloadFormatters(context: vscode.ExtensionContext): Promise<vscode.Disposable[]> {
-  let disposables = [];
+  let disposables: vscode.Disposable[] = [];
 
   if (!extensionConfiguration("formatting").enabled) {
     return disposables;
@@ -46,7 +45,7 @@ async function reloadFormatters(context: vscode.ExtensionContext): Promise<vscod
 
   const sub = vscode.languages.registerDocumentFormattingEditProvider('meson', {
     async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-      return await props.format(tool, document);
+      return await props.format(tool!, document);
     }
   })
 

src/introspection.ts
@@ -27,7 +27,7 @@ async function introspectMeson<T>(buildDir: string, filename: string, introspect
 export async function getMesonTargets(buildDir: string) {
   const parsed = await introspectMeson<Targets>(buildDir, "intro-targets.json", "--targets");
 
-  if (getMesonVersion()[1] < 50) {
+  if ((await getMesonVersion())[1] < 50) {
     return parsed.map(t => {
       if (typeof t.filename === "string") t.filename = [t.filename]; // Old versions would directly pass a string with only 1 filename on the target
       return t;

src/linters.ts
@@ -4,6 +4,7 @@ import {
   getOutputChannel,
 } from "./utils";
 import {
+  ExtensionConfiguration,
   LinterConfiguration,
   ToolCheckFunc,
   Tool
@@ -21,29 +22,30 @@ type LinterDefinition = {
   check: ToolCheckFunc
 }
 
-const linters: Record<string, LinterDefinition> = {
+const linters: Record<keyof ExtensionConfiguration["linter"], LinterDefinition> = {
   muon: {
     lint: muon.lint,
     check: muon.check,
   }
 }
 
-async function reloadLinters(sourceRoot: string, context: vscode.ExtensionContext, diagnostics: vscode.DiagnosticCollection): Promise<vscode.Disposable[]> {
-  let disposables = [];
+async function reloadLinters(sourceRoot: string, context: vscode.ExtensionContext, diagnostics: vscode.DiagnosticCollection) {
+  let disposables: vscode.Disposable[] = [];
 
   if (!extensionConfiguration("linting").enabled) {
     return disposables;
   }
 
-  let enabled_linters: ((document: vscode.TextDocument) => Promise<vscode.Diagnostic[]>)[] = [];
+  let enabledLinters: ((document: vscode.TextDocument) => Promise<vscode.Diagnostic[]>)[] = [];
+  let name: keyof typeof linters;
 
-  for (const [name, props] of Object.entries(linters)) {
+  for (name in linters) {
     const config: LinterConfiguration = extensionConfiguration("linter")[name];
-
     if (!config.enabled) {
       continue;
     }
 
+    const props = linters[name];
     const { tool, error } = await props.check();
     if (error) {
       getOutputChannel().appendLine(`Failed to enable linter ${name}: ${error}`)
@@ -51,8 +53,8 @@ async function reloadLinters(sourceRoot: string, context: vscode.ExtensionContex
       continue;
     }
 
-    const linter = async (document: vscode.TextDocument) => await props.lint(tool, sourceRoot, document)
-    enabled_linters.push(linter);
+    const linter = async (document: vscode.TextDocument) => await props.lint(tool!, sourceRoot, document)
+    enabledLinters.push(linter);
   }
 
   const lintAll = (document: vscode.TextDocument) => {
@@ -60,7 +62,7 @@ async function reloadLinters(sourceRoot: string, context: vscode.ExtensionContex
       return;
     }
 
-    Promise.all(enabled_linters.map(l => l(document))).then(values => {
+    Promise.all(enabledLinters.map(l => l(document))).then(values => {
       diagnostics.set(document.uri, values.flat());
     })
   }

src/tests.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import {
+  ExecResult,
   exec,
   extensionConfiguration,
   workspaceRelative,
@@ -28,7 +29,7 @@ export async function rebuildTests(controller: vscode.TestController) {
 }
 
 export async function testRunHandler(controller: vscode.TestController, request: vscode.TestRunRequest, token: vscode.CancellationToken) {
-  const run = controller.createTestRun(request, null, false);
+  const run = controller.createTestRun(request, undefined, false);
   const queue: vscode.TestItem[] = [];
 
   if (request.include) {
@@ -45,13 +46,15 @@ export async function testRunHandler(controller: vscode.TestController, request:
       let duration = Date.now() - starttime;
       run.passed(test, duration);
     } catch (e) {
-      run.appendOutput(e.stdout);
+      const execResult = e as ExecResult;
+
+      run.appendOutput(execResult.stdout);
       let duration = Date.now() - starttime;
-      if (e.error.code == 125) {
+      if (execResult.error?.code == 125) {
         vscode.window.showErrorMessage("Failed to build tests. Results will not be updated");
-        run.errored(test, new vscode.TestMessage(e.stderr));
+        run.errored(test, new vscode.TestMessage(execResult.stderr));
       } else {
-        run.failed(test, new vscode.TestMessage(e.stderr), duration);
+        run.failed(test, new vscode.TestMessage(execResult.stderr), duration);
       }
     }
   }
@@ -60,7 +63,7 @@ export async function testRunHandler(controller: vscode.TestController, request:
 }
 
 export async function testDebugHandler(controller: vscode.TestController, request: vscode.TestRunRequest, token: vscode.CancellationToken) {
-  const run = controller.createTestRun(request, null, false);
+  const run = controller.createTestRun(request, undefined, false);
   const queue: vscode.TestItem[] = [];
 
   if (request.include) {

src/tools/muon.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import * as cp from 'child_process';
 import {
+  ExecResult,
   exec,
   execFeed,
   extensionConfiguration,
@@ -15,7 +15,7 @@ export async function lint(
   root: string,
   document: vscode.TextDocument
 ): Promise<vscode.Diagnostic[]> {
-  const { error, stdout, stderr } = await execFeed(
+  const { stderr } = await execFeed(
     muon.path,
     ["analyze", "-l", "-O", document.uri.fsPath],
     { cwd: root },
@@ -87,24 +87,22 @@ export async function format(
   return [new vscode.TextEdit(documentRange, stdout)];
 }
 
-export async function check(): Promise<{ tool: Tool, error: string }> {
+export async function check(): Promise<{ tool?: Tool, error?: string }> {
   const muon_path = extensionConfiguration("muonPath");
-
-  const undef_muon = { path: undefined, version: undefined };
-
   let stdout: string, stderr: string;
 
   try {
-    ({ stdout, stderr } = await exec(muon_path, ["version"]))
-  } catch (exception) {
-    const { error, stdout, stderr }: { error: cp.ExecException, stdout: string, stderr: string } = exception;
+    ({ stdout, stderr } = await exec(muon_path, ["version"]));
+  }
+  catch (e) {
+    const { error, stdout, stderr } = e as ExecResult;
     console.log(error);
-    return { tool: undef_muon, error: error.message };
+    return { error: error!.message };
   }
 
   const line1 = stdout.split("\n")[0].split(" ")
   if (line1.length !== 2) {
-    return { tool: undef_muon, error: `Invalid version string: ${line1}` };
+    return { error: `Invalid version string: ${line1}` };
   }
 
   const ver = line1[1].split("-")[0].split('.').map((s) => {
@@ -115,5 +113,5 @@ export async function check(): Promise<{ tool: Tool, error: string }> {
     return Number.parseInt(s)
   }) as [number, number, number];
 
-  return { tool: { path: muon_path, version: ver }, error: undefined };
+  return { tool: { path: muon_path, version: ver } };
 }

commit 72926e55bf23564c0901f971609939f35726734c

Fix boolean logic of filter in getTask().
Broken by de6d4e91c7f8c5c74c5fb3756937c7cb1a37d847.

src/tasks.ts
@@ -179,7 +179,7 @@ export async function getMesonTasks(buildDir: string) {
 export async function getTask(mode: string, name?: string) {
   const tasks = await vscode.tasks.fetchTasks({ type: "meson" });
   const filtered = tasks.filter(
-    (t) => t.definition["mode"] === mode && !name || t.definition["target"] === name
+    (t) => t.definition["mode"] == mode && (!name || t.definition["target"] == name)
   );
   if (filtered.length === 0) {
     throw new Error(`Cannot find ${mode} target ${name}.`);