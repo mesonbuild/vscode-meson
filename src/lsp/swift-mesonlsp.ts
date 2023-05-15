60dc5f7333c893d8c1715a39f6dcc126d35139ea
JCWasmx86 <JCWasmx86@t-online.de>
2023-05-15 06:18:27 +0200

    Add integration for Swift-MesonLSP
    Co-Authored-By: Tristan Partin <tristan@partin.io>

---
+++ src/lsp/swift-mesonlsp.ts
@@ -0,0 +1,71 @@
+import * as os from "os";
+import * as vscode from "vscode";
+
+import { LanguageServerClient } from "../lsp";
+import { Executable } from "vscode-languageclient/node";
+
+export class SwiftMesonLspLanguageClient extends LanguageServerClient {
+  private static artifacts: { [key: string]: { name: string; hash: string } } = {
+    "win32-x64": {
+      name: "Swift-MesonLSP-win64.zip",
+      hash: "81e4c433623b344d8e321a0e48426b3f83844b108d099a8cac6993c546084b66",
+    },
+    "darwin-x64": {
+      name: "Swift-MesonLSP-macos12.zip",
+      hash: "9073875ba3eb75fe0c2123e4d0e09c8bccde271c907e74fc84110c85662e4a49",
+    },
+    "linux-x64": {
+      name: "Swift-MesonLSP.zip",
+      hash: "2b9359e9cff8cec40279f78c62ef8eb94693e26e21716f82293cf96ad1f723aa",
+    },
+  };
+
+  static override repoURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP";
+  static override setupURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP/tree/main/Docs";
+  static override version: string = "2.4.4";
+
+  get runExe(): Executable {
+    return {
+      command: this.languageServerPath!.fsPath,
+      args: ["--lsp"],
+    };
+  }
+
+  get debugExe(): Executable {
+    return {
+      command: this.languageServerPath!.fsPath,
+      args: ["--lsp"],
+    };
+  }
+
+  constructor(languageServerPath: vscode.Uri, context: vscode.ExtensionContext) {
+    super("Swift-MesonLSP", languageServerPath, context);
+  }
+
+  static override artifact(): { url: string; hash: string } | null {
+    const arch = os.arch();
+    const platform = os.platform();
+    if (arch !== "x64") return null;
+
+    const artifact = SwiftMesonLspLanguageClient.artifacts[`${platform}-${arch}`];
+    return {
+      url: `${SwiftMesonLspLanguageClient.repoURL}/releases/download/v${SwiftMesonLspLanguageClient.version}/${artifact.name}`,
+      hash: artifact.hash,
+    };
+  }
+
+  static override supportsSystem(): boolean {
+    const arch = os.arch();
+    if (arch != "x64") return false;
+
+    const platform = os.platform();
+    switch (platform) {
+      case "darwin":
+      case "linux":
+      case "win32":
+        return true;
+      default:
+        return false;
+    }
+  }
+}
