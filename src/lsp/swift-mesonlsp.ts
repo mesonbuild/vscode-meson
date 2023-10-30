f1c88a2c98ab21727d432c204903ef7a1476a7cc
JCWasmx86 <JCWasmx86@t-online.de>
2023-10-30 06:30:44 +0100

    Add support for Apple Silicon to Swift-MesonLSP

--- src/lsp/swift-mesonlsp.ts
+++ src/lsp/swift-mesonlsp.ts
@@ -14,6 +14,10 @@ export class SwiftMesonLspLanguageClient extends LanguageServerClient {
       name: "Swift-MesonLSP-macos12.zip",
       hash: "f7d12dcecf60f0d61993f952dd3d43ab33abe5b87cc5c72ec7904805e3f902a1",
     },
+    "darwin-arm64": {
+      name: "Swift-MesonLSP-macos12.zip",
+      hash: "024457775bbaa55ce1a0c1ba65a8f92c4c7879ee42c7d7259f131bbb02fb3fa2",
+    },
     "linux-x64": {
       name: "Swift-MesonLSP.zip",
       hash: "77f3b01b59992d1e4e2775c05dc74d60a4cd34a99085a6fd9663ef0d698c3270",
@@ -45,9 +49,11 @@ export class SwiftMesonLspLanguageClient extends LanguageServerClient {
   static override artifact(): { url: string; hash: string } | null {
     const arch = os.arch();
     const platform = os.platform();
-    if (arch !== "x64") return null;
+    const slug = `${platform}-${arch}`;
 
-    const artifact = SwiftMesonLspLanguageClient.artifacts[`${platform}-${arch}`];
+    if (arch !== "x64" && slug != "darwin-arm64") return null;
+
+    const artifact = SwiftMesonLspLanguageClient.artifacts[slug];
     return {
       url: `${SwiftMesonLspLanguageClient.repoURL}/releases/download/v${SwiftMesonLspLanguageClient.version}/${artifact.name}`,
       hash: artifact.hash,
@@ -56,14 +62,17 @@ export class SwiftMesonLspLanguageClient extends LanguageServerClient {
 
   static override supportsSystem(): boolean {
     const arch = os.arch();
-    if (arch != "x64") return false;
+    if (arch != "x64" && arch != "arm64") return false;
 
     const platform = os.platform();
     switch (platform) {
       case "darwin":
+        // x64 and ARM are supported thanks to universal binaries.
+        return true;
       case "linux":
       case "win32":
-        return true;
+        // Currently no support for ARM on other operating systems.
+        return arch == "x64";
       default:
         return false;
     }
