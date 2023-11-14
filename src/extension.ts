mesonbuild/vscode-meson

commit 60dc5f7333c893d8c1715a39f6dcc126d35139ea

Add integration for Swift-MesonLSP
Co-Authored-By: Tristan Partin <tristan@partin.io>

src/extension.ts
@@ -10,12 +10,14 @@ import {
   useCompileCommands,
   clearCache,
   checkMesonIsConfigured,
+  getOutputChannel,
 } from "./utils";
 import { DebugConfigurationProvider } from "./configprovider";
 import { testDebugHandler, testRunHandler, rebuildTests } from "./tests";
 import { activateLinters } from "./linters";
 import { activateFormatters } from "./formatters";
-import { TaskQuickPickItem } from "./types";
+import { SettingsKey, TaskQuickPickItem } from "./types";
+import { createLanguageServerClient } from "./lsp/common";
 
 export let extensionPath: string;
 let explorer: MesonProjectExplorer;
@@ -33,9 +35,6 @@ export async function activate(ctx: vscode.ExtensionContext) {
 
   const root = vscode.workspace.workspaceFolders[0].uri.fsPath;
   const buildDir = workspaceRelative(extensionConfiguration("buildFolder"));
-
-  activateLinters(root, ctx);
-  activateFormatters(ctx);
 
   explorer = new MesonProjectExplorer(ctx, root, buildDir);
 
@@ -176,8 +175,7 @@ export async function activate(ctx: vscode.ExtensionContext) {
   );
 
   if (!checkMesonIsConfigured(buildDir)) {
-    const configureOnOpenKey = "configureOnOpen";
-    let configureOnOpen = extensionConfiguration(configureOnOpenKey);
+    let configureOnOpen = extensionConfiguration(SettingsKey.configureOnOpen);
     if (configureOnOpen === "ask") {
       enum Options {
         yes = "Yes",
@@ -196,7 +194,7 @@ export async function activate(ctx: vscode.ExtensionContext) {
           break;
 
         case Options.never:
-          extensionConfigurationSet(configureOnOpenKey, false, vscode.ConfigurationTarget.Workspace);
+          extensionConfigurationSet(SettingsKey.configureOnOpen, false, vscode.ConfigurationTarget.Workspace);
           break;
 
         case Options.yes:
@@ -204,16 +202,71 @@ export async function activate(ctx: vscode.ExtensionContext) {
           break;
 
         case Options.always:
-          extensionConfigurationSet(configureOnOpenKey, true, vscode.ConfigurationTarget.Workspace);
+          extensionConfigurationSet(SettingsKey.configureOnOpen, true, vscode.ConfigurationTarget.Workspace);
           configureOnOpen = true;
           break;
       }
     }
-
-    if (configureOnOpen === true) {
-      runFirstTask("reconfigure");
-    }
   }
+
+  const downloadLanguageServer = extensionConfiguration(SettingsKey.downloadLanguageServer);
+  const server = extensionConfiguration(SettingsKey.languageServer);
+  const shouldDownload = async (downloadLanguageServer: boolean | "ask"): Promise<boolean> => {
+    if (typeof downloadLanguageServer === "boolean") return downloadLanguageServer;
+
+    enum Options {
+      yes = "Yes",
+      no = "Not this time",
+      never = "Never",
+    }
+
+    const response = await vscode.window.showInformationMessage(
+      "Should the extension try to download the language server?",
+      ...Object.values(Options),
+    );
+
+    switch (response) {
+      case Options.yes:
+        extensionConfigurationSet(SettingsKey.downloadLanguageServer, true, vscode.ConfigurationTarget.Global);
+        return true;
+
+      case Options.never:
+        extensionConfigurationSet(SettingsKey.downloadLanguageServer, false, vscode.ConfigurationTarget.Global);
+        return false;
+
+      case Options.no:
+        extensionConfigurationSet(SettingsKey.downloadLanguageServer, "ask", vscode.ConfigurationTarget.Global);
+        return false;
+    }
+
+    return false;
+  };
+
+  let client = await createLanguageServerClient(server, await shouldDownload(downloadLanguageServer), ctx);
+  if (client !== null && server == "Swift-MesonLSP") {
+    ctx.subscriptions.push(client);
+    client.start();
+
+    getOutputChannel().appendLine("Not enabling the muon linter/formatter because Swift-MesonLSP is active.");
+  } else {
+    activateLinters(root, ctx);
+    activateFormatters(ctx);
+  }
+
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("mesonbuild.restartLanguageServer", async () => {
+      if (client === null) {
+        client = await createLanguageServerClient(server, await shouldDownload(downloadLanguageServer), ctx);
+        if (client !== null) {
+          ctx.subscriptions.push(client);
+          client.start();
+          // TODO: The output line from above about not enabling muon would be good to have here.
+        }
+      } else {
+        client.restart();
+      }
+    }),
+  );
 
   async function pickTask(mode: string) {
     const picker = vscode.window.createQuickPick<TaskQuickPickItem>();

src/lsp/common.ts
@@ -1,0 +1,58 @@
+import * as vscode from "vscode";
+import { LanguageServerClient } from ".";
+import { LanguageServer } from "../types";
+import { SwiftMesonLspLanguageClient } from "./swift-mesonlsp";
+import { Uri } from "vscode";
+
+export async function createLanguageServerClient(
+  server: LanguageServer,
+  download: boolean,
+  context: vscode.ExtensionContext,
+): Promise<LanguageServerClient | null> {
+  const serverToClass = (server: LanguageServer) => {
+    switch (server) {
+      case "Swift-MesonLSP":
+        return SwiftMesonLspLanguageClient;
+      default:
+        return null;
+    }
+  };
+
+  const klass = serverToClass(server);
+  if (klass == null) {
+    return null;
+  }
+  if (!klass.supportsSystem()) {
+    vscode.window.showErrorMessage("The configured language server does not support the current system.");
+    return null;
+  }
+
+  let languageServerPath = LanguageServerClient.resolveLanguageServerPath(server, context);
+  if (languageServerPath === null) {
+    if (klass.artifact() == null) {
+      enum Options {
+        open = "Open documentation in browser",
+      }
+      const response = await vscode.window.showErrorMessage(
+        "This language server supports your systen, but provides no artifacts for automatic setup",
+        ...Object.values(Options),
+      );
+      if (response == Options.open) {
+        vscode.env.openExternal(Uri.parse(klass.setupURL));
+      }
+      return null;
+    }
+    if (download) {
+      languageServerPath = await klass.download(server, klass.version, context);
+      if (languageServerPath === null) {
+        vscode.window.showErrorMessage("Failed to download the language server.");
+        return null;
+      }
+    } else {
+      vscode.window.showErrorMessage("Failed to find a language server on the system.");
+      return null;
+    }
+  }
+
+  return new klass(languageServerPath, context);
+}

src/lsp/index.ts
@@ -1,0 +1,196 @@
+import * as Admzip from "adm-zip";
+import * as which from "which";
+import * as https from "https";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+import * as storage from "../storage";
+import { LanguageServer } from "../types";
+
+export abstract class LanguageServerClient {
+  private static readonly clientOptions: LanguageClientOptions = {
+    documentSelector: ["meson", { scheme: "file", language: "meson" }],
+  };
+  private ls: LanguageClient | null = null;
+  private readonly context: vscode.ExtensionContext;
+
+  protected languageServerPath: vscode.Uri | null;
+  readonly server: LanguageServer;
+
+  static readonly repoURL: string;
+  static readonly setupURL: string;
+  static readonly version: string;
+
+  protected abstract get debugExe(): Executable;
+  protected abstract get runExe(): Executable;
+
+  protected constructor(server: LanguageServer, languageServerPath: vscode.Uri, context: vscode.ExtensionContext) {
+    this.server = server;
+    this.languageServerPath = languageServerPath;
+    this.context = context;
+  }
+
+  private static cachedLanguageServer(server: LanguageServer, context: vscode.ExtensionContext): vscode.Uri | null {
+    const uri = vscode.Uri.joinPath(
+      storage.uri(storage.Location.LSP, context),
+      `${server}${os.platform() === "win32" ? ".exe" : ""}`,
+    );
+
+    return fs.existsSync(uri.fsPath) ? uri : null;
+  }
+
+  private static async computeFileHash(filePath: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const hash = crypto.createHash("sha256");
+      const stream = fs.createReadStream(filePath);
+
+      stream.on("error", reject);
+
+      stream.on("data", (data) => {
+        hash.update(data);
+      });
+
+      stream.on("end", () => {
+        resolve(hash.digest("hex"));
+      });
+    });
+  }
+
+  private static async fetch(url: string, dest: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const file = fs.createWriteStream(dest);
+      const request = https.get(url, (response) => {
+        if (response.statusCode === 302 || response.statusCode === 301 || response.statusCode === 302) {
+          LanguageServerClient.fetch(response.headers.location!, dest).then(resolve).catch(reject);
+        } else {
+          response.pipe(file);
+
+          file.on("finish", () => {
+            file.close((_) => {
+              resolve();
+            });
+          });
+
+          file.on("error", (err) => {
+            vscode.window.showErrorMessage(`Error writing to file: ${err}`);
+            reject(err);
+          });
+        }
+      });
+
+      request.on("error", (err) => {
+        vscode.window.showErrorMessage(`Request error: ${err}`);
+        reject(err);
+      });
+    });
+  }
+
+  static async download(
+    server: LanguageServer,
+    version: string,
+    context: vscode.ExtensionContext,
+  ): Promise<vscode.Uri | null> {
+    const lspDir = storage.uri(storage.Location.LSP, context).fsPath;
+    const artifact = this.artifact();
+    if (artifact === null) return null;
+
+    fs.rmSync(lspDir, { recursive: true, force: true });
+    fs.mkdirSync(lspDir, { recursive: true });
+
+    let uri: vscode.Uri | null = null;
+    const tmpPath = path.join(os.tmpdir(), `vscode-meson-${server}-${Date.now()}.zip`);
+
+    try {
+      await LanguageServerClient.fetch(artifact.url, tmpPath);
+      let hash = await this.computeFileHash(tmpPath);
+      if (hash !== artifact.hash) {
+        vscode.window.showErrorMessage(`Invalid hash: Expected ${artifact.hash}, got ${hash}.`);
+        fs.unlinkSync(tmpPath);
+        return null;
+      }
+
+      const zip = new Admzip(tmpPath);
+      zip.extractAllTo(lspDir);
+      const binary = path.join(lspDir, server!);
+      if (os.platform() != "win32") fs.chmodSync(binary, 0o755);
+      const versionFile = path.join(lspDir, "version");
+      fs.writeFileSync(versionFile, version);
+
+      vscode.window.showInformationMessage("Language server was downloaded.");
+      uri = vscode.Uri.from({ scheme: "file", path: binary });
+      fs.unlinkSync(tmpPath);
+      return uri;
+    } catch (err) {
+      vscode.window.showErrorMessage(JSON.stringify(err));
+      fs.unlinkSync(tmpPath);
+    }
+    return uri;
+  }
+
+  static resolveLanguageServerPath(server: LanguageServer, context: vscode.ExtensionContext): vscode.Uri | null {
+    const config = vscode.workspace.getConfiguration("mesonbuild");
+    if (config["languageServerPath"] !== null && config["languageServerPath"] != "")
+      return vscode.Uri.from({ scheme: "file", path: config["languageServerPath"] });
+
+    const cached = LanguageServerClient.cachedLanguageServer(server, context);
+    if (cached !== null) return cached;
+
+    const binary = which.sync(server!, { nothrow: true });
+    if (binary !== null) return vscode.Uri.from({ scheme: "file", path: binary });
+
+    return null;
+  }
+
+  protected static supportsSystem(): boolean {
+    return true;
+  }
+
+  protected static artifact(): { url: string; hash: string } | null {
+    return null;
+  }
+
+  dispose() {
+    if (this.ls !== null) {
+      this.ls.stop();
+      this.ls = null;
+    }
+  }
+
+  restart(): void {
+    this.dispose();
+    this.languageServerPath = LanguageServerClient.resolveLanguageServerPath(this.server, this.context);
+    if (this.languageServerPath === null) {
+      vscode.window.showErrorMessage(
+        "Failed to restart the language server because a binary was not found and could not be downloaded",
+      );
+    } else {
+      this.start();
+    }
+  }
+
+  start(): void {
+    const serverOptions: ServerOptions = {
+      run: this.runExe,
+      debug: this.debugExe,
+      transport: TransportKind.stdio,
+    };
+
+    this.ls = new LanguageClient(
+      this.server!,
+      `Meson Language Server (${this.server})`,
+      serverOptions,
+      LanguageServerClient.clientOptions,
+      true,
+    );
+    this.ls.start();
+  }
+}

src/lsp/swift-mesonlsp.ts
@@ -1,0 +1,71 @@
+import * as os from "os";
+import * as vscode from "vscode";
+
+import { LanguageServerClient } from "../lsp";
+import { Executable } from "vscode-languageclient/node";
+
+export class SwiftMesonLspLanguageClient extends LanguageServerClient {
+  private static artifacts: { [key: string]: { name: string; hash: string } } = {
+    "win32-x64": {
+      name: "Swift-MesonLSP-win64.zip",
+      hash: "81e4c433623b344d8e321a0e48426b3f83844b108d099a8cac6993c546084b66",
+    },
+    "darwin-x64": {
+      name: "Swift-MesonLSP-macos12.zip",
+      hash: "9073875ba3eb75fe0c2123e4d0e09c8bccde271c907e74fc84110c85662e4a49",
+    },
+    "linux-x64": {
+      name: "Swift-MesonLSP.zip",
+      hash: "2b9359e9cff8cec40279f78c62ef8eb94693e26e21716f82293cf96ad1f723aa",
+    },
+  };
+
+  static override repoURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP";
+  static override setupURL: string = "https://github.com/JCWasmx86/Swift-MesonLSP/tree/main/Docs";
+  static override version: string = "2.4.4";
+
+  get runExe(): Executable {
+    return {
+      command: this.languageServerPath!.fsPath,
+      args: ["--lsp"],
+    };
+  }
+
+  get debugExe(): Executable {
+    return {
+      command: this.languageServerPath!.fsPath,
+      args: ["--lsp"],
+    };
+  }
+
+  constructor(languageServerPath: vscode.Uri, context: vscode.ExtensionContext) {
+    super("Swift-MesonLSP", languageServerPath, context);
+  }
+
+  static override artifact(): { url: string; hash: string } | null {
+    const arch = os.arch();
+    const platform = os.platform();
+    if (arch !== "x64") return null;
+
+    const artifact = SwiftMesonLspLanguageClient.artifacts[`${platform}-${arch}`];
+    return {
+      url: `${SwiftMesonLspLanguageClient.repoURL}/releases/download/v${SwiftMesonLspLanguageClient.version}/${artifact.name}`,
+      hash: artifact.hash,
+    };
+  }
+
+  static override supportsSystem(): boolean {
+    const arch = os.arch();
+    if (arch != "x64") return false;
+
+    const platform = os.platform();
+    switch (platform) {
+      case "darwin":
+      case "linux":
+      case "win32":
+        return true;
+      default:
+        return false;
+    }
+  }
+}

src/storage.ts
@@ -1,0 +1,9 @@
+import * as vscode from "vscode";
+
+export enum Location {
+  LSP = "lsp",
+}
+
+export function uri(location: Location, context: vscode.ExtensionContext): vscode.Uri {
+  return vscode.Uri.joinPath(context.globalStorageUri, location);
+}

src/types.ts
@@ -8,6 +8,8 @@ export type LinterConfiguration = {
   enabled: boolean;
 };
 
+export type LanguageServer = "Swift-MesonLSP" | null;
+
 export interface ExtensionConfiguration {
   configureOnOpen: boolean | "ask";
   configureOptions: string[];
@@ -27,6 +29,9 @@ export interface ExtensionConfiguration {
     muonConfig: string | null;
   };
   debugOptions: object;
+  languageServer: LanguageServer;
+  languageServerPath: string;
+  downloadLanguageServer: boolean | "ask";
 }
 
 export interface TaskQuickPickItem extends vscode.QuickPickItem {
@@ -119,3 +124,9 @@ export type Targets = Target[];
 export type BuildOptions = BuildOption<any>[];
 export type Dependencies = Dependency[];
 export type Tests = Test[];
+
+export enum SettingsKey {
+  downloadLanguageServer = "downloadLanguageServer",
+  languageServer = "languageServer",
+  configureOnOpen = "configureOnOpen",
+}

commit 8cf1371c661b435c8a210f9b6cb7bb6ca941020b

Fix regression introduced in 60dc5f7333.

src/extension.ts
@@ -207,6 +207,10 @@ export async function activate(ctx: vscode.ExtensionContext) {
           break;
       }
     }
+
+    if (configureOnOpen) {
+      runFirstTask("reconfigure");
+    }
   } else {
     await rebuildTests(controller);
   }