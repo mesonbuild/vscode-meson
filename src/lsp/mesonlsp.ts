6160192fa19a0f5fdb7bd609d5557cf1befae520
JCWasmx86 <JCWasmx86@t-online.de>
2024-08-04 14:15:02 +0200

    Sort keys according to the filename
    This improves the clarity and reduces the probability
    of errors while updating the checksums

--- src/lsp/mesonlsp.ts
+++ src/lsp/mesonlsp.ts
@@ -6,25 +6,25 @@ import { LanguageServerClient } from "../lsp";
 
 export class MesonLSPLanguageClient extends LanguageServerClient {
   private static artifacts: { [key: string]: { name: string; hash: string } } = {
-    "win32-x64": {
-      name: "mesonlsp-x86_64-pc-windows-gnu.zip",
-      hash: "b5d6c1f414a938ccaacc93cdb2bc2f7f126da7fe52e99e25d8f94b60ac82ca56",
+    "darwin-arm64": {
+      name: "mesonlsp-aarch64-apple-darwin.zip",
+      hash: "c8bc02ffca66d0a1bef972e5f884005cc72efe79347fbe35a44da55c53c01de3",
+    },
+    "linux-arm64": {
+      name: "mesonlsp-aarch64-unknown-linux-musl.zip",
+      hash: "29db76675e02f33b70c18a8088c39c365b7c315509bd176ce32958a2c25eb442",
     },
     "darwin-x64": {
       name: "mesonlsp-x86_64-apple-darwin.zip",
       hash: "0da55be0ab6e178bf8953808db4333521d6694bcc78c2ea8b41f3907f133d3b2",
     },
-    "darwin-arm64": {
-      name: "mesonlsp-aarch64-apple-darwin.zip",
-      hash: "c8bc02ffca66d0a1bef972e5f884005cc72efe79347fbe35a44da55c53c01de3",
+    "win32-x64": {
+      name: "mesonlsp-x86_64-pc-windows-gnu.zip",
+      hash: "b5d6c1f414a938ccaacc93cdb2bc2f7f126da7fe52e99e25d8f94b60ac82ca56",
     },
     "linux-x64": {
       name: "mesonlsp-x86_64-unknown-linux-musl.zip",
       hash: "76dcd3b5a04dc4bcbfb1d2c706e755738eb1eb3ab611006285902e544b6ab6b0",
-    },
-    "linux-arm64": {
-      name: "mesonlsp-aarch64-unknown-linux-musl.zip",
-      hash: "29db76675e02f33b70c18a8088c39c365b7c315509bd176ce32958a2c25eb442",
     },
   };
 
